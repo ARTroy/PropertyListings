568812bb9f146b9c76e866b7931d9420586cedee
Alexander Troy <alext3hgr8@gmail.com>
2016-07-10 16:27:57 +0100

    some changes based on auth make

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -5,8 +5,36 @@
     </head>
     <body>
     	<div class="header">
+            <div class="nav">
+                <div class="row">
+                    <div class="small-6 columns">
+                    </div>
+                    <div class="medium-6 columns">
+                        <div class="row">
+                            <form method="POST" action="{{action('AuthController@login')}}">
+                            {!! csrf_field() !!}
+                            <div class="medium-5 columns">
+                                <label>Email
+                                    <input type="email" name="email">
+                                </label>
+                            </div>
+                            <div class="medium-5 columns">
+                                <label>Password
+                                    <input type="password" name="password">
+                                </label>
+                            </div>
+                            <div class="medium-5 columns">
+                                
+                            </div>
+                            <button type="submit">Login</button>                   
+                            </form>
+                        </div>
+                    </div>    
+                </div>
+               
+            </div>
             <div class="row">
-                <div class="small-12">
+                <div class="small-12 columns">
     		        @include('common.errors')
                 </div>
             </div>
