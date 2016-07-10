8ba85a6b212e69664758ac78b7e5cbfec96c0b84
Alexander Troy <alext3hgr8@gmail.com>
2016-07-10 01:55:35 +0100

    changes

--- database/migrations/2016_07_03_100923_create_users_table.php
+++ database/migrations/2016_07_03_100923_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('api_token')->nullable();
+            $table->boolean('admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
