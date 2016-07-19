df6ea1041c52bc399c5268a94eb8b405c7a7b2a7
Alexander Troy <alext3hgr8@gmail.com>
2016-07-20 00:37:53 +0100

    good progress

--- resources/views/user/property/create.blade.php
+++ resources/views/user/property/create.blade.php
@@ -7,25 +7,25 @@
 		<div class="small-12 columns">
 			<div class='row divide_row'>
 				<div class="small-4 medium-4 columns">
-					<span class='badge' style="background-color:#65ae87; color:white; font-size:25px; margin-bottom:10px">1</span>
+					<span class='badge badge_style' style="background-color:#65ae87; color:white; font-size:25px; margin-bottom:10px">1</span>
 					<p  class="alternative">Choose the industry you are selling to</p>
 				</div>
 				<div class="small-8 medium-8 columns">
 					<div class="row collapse buttong">
 						<div class="small-12 medium-6 columns ">
 							<button id='resi' type='button' class='button selected' 
-							style="font-size:18px; width: 100%;">Residential</button>
+							>Residential</button>
 						</div>
 						<div class="small-12 medium-6 columns">
 							<button id='comm' type='button' class='button' 
-							style="font-size:18px; width: 100%;">Commercial</button>
+							>Commercial</button>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class='row divide_row'>
 				<div  class="small-4 medium-4 columns">
-					<span class="badge" style="background-color:#65ae87; color:white; font-size:25px; margin-bottom:10px">2</span>
+					<span class="badge badge_style" style="background-color:#65ae87; color:white; font-size:25px; margin-bottom:10px">2</span>
 					<p  class="alternative">Choose the type of property</p>
 				</div>
 				<div class="small-8 medium-8 columns">
@@ -51,28 +51,28 @@
 							</div>
 						@endforeach
 					</div>
-
-					{{--
-					<div class="switch" id='resi_select'>
-						<input class="switch-input" id="resi_{{$type->id}}" type="radio" name="property_type" value='{{$type->type_name}}'>
-						<label class="switch-paddle" for="resi_{{$type->id}}">
-							<span class="show-for-sr"></span>
-						</label>
-					</div>
-
-					<select style="display:none" name='property_type' >
-						@foreach($residential as $type)
-							<option value='{{$type->id}}'>{{$type->type_name}}</option>
-						@endforeach
-					</select>
-					--}}
 				</div>
 			</div>
-			<div class='small-12 columns'>
-				<button class="file-upload">            
-						<input type="file" class="file-input">
-					</button>
-
+			<div class='row divide_row'>
+				<div class='small-4 columns'>
+					<span class="badge badge_style">3</span>
+					<p  class="alternative">Add a title and image</p>
+				</div>
+				<div class="small-8 medium-8 columns">
+					<div class="row">
+						<div class="small-12 medium-6 columns">
+							<label>Title <input type="text" name="title"></label>
+						</div>
+						<div class="small-12 medium-6 columns">
+							<label>Image 
+								<button class="file-upload small-12">            
+									<input type="file" class="file-input">
+								</button>
+							</label>
+						</div>
+					</div>
+					
+				</div>
 			</div>
 		</div>
 	</div>	
